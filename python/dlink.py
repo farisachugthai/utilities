0be3a510d5e61a9a22a28440869fa4bbf32719d0
farisachugthai <farischugthai@gmail.com>
2018-06-01 09:51:01 -0400

    The directory linker works the exact way I want it to!

--- python/dlink.py
+++ python/dlink.py
@@ -3,9 +3,12 @@
 import os
 import sys
 
-# TODO: If you want to add some zest, import glob and let users wildcard it
-# TODO: Get the global variables out of here. They don't need to be used if
-# this module was imported so they belong in if __name__...
+# TODO #1: Test that the src dir [the one we're creating symlinks in] is handled
+# correctly as an optional parameter. AKA drop it and see if things still work.
+# Don't think that it will for the time being because of the way dest is
+# defined. It needs to account for different lengths of sys.argv the way
+# src does.
+# TODO #2 : If you want to add some zest, import glob and let users wildcard it
 
 cwd = os.path.join(os.getcwd(), '')
 src = sys.argv[-1] if len(sys.argv) == 3 else cwd
@@ -16,7 +19,7 @@ dest = sys.argv[-2]
 
 print("Your variable src is: " + src + " and type: " + str(type(src)))
 print("Your variable dest is: " + dest + " and type: " + str(type(dest)))
-print(os.listdir(dest))
+print("The files that we'll be symlinking to are: " + str(os.listdir(dest)))
 
 
 def dlink(dest, src=cwd):
@@ -33,11 +36,8 @@ def dlink(dest, src=cwd):
     """
 
     for i in os.listdir(dest):
-        print(i)
         dest_file = os.path.join(dest, i)
-        print(dest_file)
         src_file = os.path.join(src, i)
-        print(src_file)
         try:
             os.symlink(dest_file, src_file)
         except FileExistsError as e:
@@ -45,11 +45,14 @@ def dlink(dest, src=cwd):
                 pass
             elif os.path.isdir(src_file):
                 pass
+            elif os.path.isfile(src_file):
+                print(src_file + " is already a file in the src dir. We will not create a symlink.")
+                pass
             else:
                 print(e)
                 sys.exit("Huh. Didn't see that coming.")
 
-        print("symlinking: " + dest + " from " + src)
+        print("symlinking: " + dest_file + " from " + src_file)
 
 
 def main():

5b31a837b246a5f9cd88e753cdf1d052dfb1653e
farisachugthai <farischugthai@gmail.com>
2018-06-05 04:09:39 -0400

    Adding in scripts for rsync.

---
+++ sh/rsync.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This is me basically taking notes as I haven't run this yet
+
+# Hoping this is the correct syntax for logical not
+if ! [[ -d "$HOME/logs" ]]; then
+    mkdir "$HOME/logs";
+    echo "Creating directory for rsync logs.";
+fi
+
+rsync -avz --recursive --update --preallocate --one-file-system --files-from="rsync.txt" \
+    --human-readable --itemize-changes --log-file="$HOME/logs/rsync$(date)" --links
+
+# Then feel free to toggle --dry-run here or there
+
+# As far as links go you have 2 options
+# -l --links means symlinks are symlinks
+# -L or --copy-links means transform symlinks into the file they refer to 
+
+# Preallocate to ensure that theres space on the EHD
+
+# -s, --protect-args -> no space-splitting; only wildcard special chars. that's interesting
+
+# Pretty straight forward. Now just to make that list of files.
+
+exit 0

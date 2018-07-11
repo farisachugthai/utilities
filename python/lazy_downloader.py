d0e43b104ca4c9ea96f58f762d6b8e0f44da6aba
farisachugthai <farischugthai@gmail.com>
2018-07-11 19:28:10 -0400

    Added a dir tree symlinker and urxvt server checker

--- python/lazy_downloader.py
+++ python/lazy_downloader.py
@@ -5,19 +5,21 @@ import requests
 import sys
 
 """
-Literally just throwing this together stream of consciousness.
 Usage:
     lazy_downloader url output_filename
 
 Must be plaintext. No MP3s or PDFs.
 If filename already exists on the system it will be overwritten.
 """
-# TODO: Dude just use Send2Trash instead of destroying their file.
 
 res = requests.get(sys.argv[1])
 res.raise_for_status()
 
-with open(sys.argv[2], "wt") as f:
+if res.headers['Content-Type']:
+    pass
+# TODO: figure out how to check that the file is plain text and not hit constant false positives
+
+with open(sys.argv[1], "wt") as f:
     f.write(res.text)
 
 # could add in a check. if the file is over a certain size use res.iter_chunk()

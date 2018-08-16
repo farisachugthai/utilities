3cafd8469847076828299f69e4fe62076175c4f9
farisachugthai <farischugthai@gmail.com>
2018-08-16 11:53:49 -0400

    Cleaning up

---
+++ TODO.md
@@ -0,0 +1,31 @@
+# TODO.md
+
+Not too dissimilar to the world's worst kanban board, this file is going to be a
+manually created index for every TODO I write in this repo.
+
+I started with a good 'hey that'd be nice to do in the future.' type project.
+
+[remove watermarks from itebook](github.com/ShadonSniper/RemoveWatermark)
+
+make a better downloader than the lazy downloader, incorporate ytdl maybe or come up wkth a gitignore downloader
+
+**08-14-18**
+
+But from here and below, it's a list of the all the TODOs I have to go. I'm sure
+there's plenty.
+
+In the official python documentation they have a HOW-TO for argparse, and I've
+been working through that slowly. As it'll be the easiest place to apply that
+knowledge, I want to refactor (python/batch_renamer.py) so that takes one
+parameter using argparse.
+
+TODO: Learn the bare minimum of std lib's tempfile module so you can add a
+helper function to create files if they don't have a batch they want to rename
+[obviously for debugging purposes]
+
+Fix up the snippets in Nvim so that I don't get blocking error messages every
+time I enter 'Insert Mode'.
+
+Either add doctests, logging or unittests [idk yet] to (python/dlink.py) so that
+you feel confident enough to begin basing (python/dot_sym.ipy) off of it and
+then begin basing (github.com/farisachugthai/newbuntu) off of that.
--- python/batch-renamer/batch_renamer.py
+++ python/batch-renamer/batch_renamer.py
@@ -15,7 +15,7 @@ Examples::
     #  img_1076.jpg --> Ashley_1.jpg
     #  img_1077.jpg --> Ashley_2.jpg
 
-Wait how much do module docstrings get indented?
+TODO: Wait how much do module docstrings get indented?
 Both the text and the quotes.
 """
 
@@ -29,10 +29,30 @@ import argparse
 import os.path
 from string import Template
 import time
+import uuid
+
+# Wth is the order up top supposed to be? I might Ijust leave it but there are docs like
+# pep 8 that get SO specific everyones code starts looking the same.
 
 
 class BatchRename(Template):
     delimiter = '%'
+
+
+def tmpfiles():
+    """Here so that if you don't want to keep botching family photos, you can
+    test this module on emporary files.
+
+    Utilize the tempfile module in order to simulate IO processes, and wrap i in a
+    context manager to ensure that all connections are closed when the script completes.
+    """
+
+# we could take a predetermined value for the range. shrugs.
+def batch_helper():
+    """Created to produce sparse and light files to accommodate testing the script."""
+for tmp0 in range(35):
+    tmp_file = open('capitalsquiz%s.txt' %(quizNum + 1), 'w') as f:
+        f.write('Arbitrary Header:\nDate'+datetime.datetime.now()
 
 
 def main(d):
@@ -42,7 +62,7 @@ def main(d):
 
     t = BatchRename(fmt)
     date = time.strftime('%d%b%y')
-    for i, filename in enumerate(os.listdir(d))0:
+    for i, filename in enumerate(os.listdir(d)):
         base, ext = os.path.splitext(filename)
         newname = t.substitute(d=date, n=i, f=ext)
         print('{0} --> {1}'.format(filename, newname))

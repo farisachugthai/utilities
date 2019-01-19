42bee80831fdafa5fc480a71a2b4b7eba41947da
farisachugthai <farischugthai@gmail.com>
2019-01-18 22:15:57 -0500

    Left over deleted files

--- sh/termux-backup-restore.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# generate installed package list
-dpkg --get-selections | cut -f1 > mypackages.txt
-
-# install packages
-xargs -0 apt install -y < <(tr \\n \\0 < mypackages.txt)

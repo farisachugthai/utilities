d13ad6526fe40c1c15480de20d354045b6ef8d34
farisachugthai <farischugthai@gmail.com>
2019-01-25 23:18:19 -0500

    Added 2 bash scripts I'm excited about. Docs.
    	new file:   sh/fzf_git.sh
    	new file:   sh/only-hdmi.sh

--- pyutil/env_checks.py
+++ pyutil/env_checks.py
@@ -4,8 +4,35 @@
 
 Easier to group similar methods in one mod then have them scattered around. 
 
+Below is a generic example of using the public methods to read in user 
+defined configurations.
+
+Example
+---------
+
+>>> from env_checks import check_xdg_config_home
+>>> if check_xdg_config_home:
+    >>> with open('module.conf') as f:
+        >>> configs = f.readlines()
+
 """
 import os
+
+
+def check_xdg_config_home():
+    """Check to see if $XDG_CONFIG_HOME has been defined.
+
+    Usage
+    ------
+    TODO
+    
+    :param: None
+    :returns: Bool
+    """
+    if os.environ.get('XDG_CONFIG_HOME'):
+        return True
+    else:
+        return False
 
 
 def check_xdg_env():
@@ -14,6 +41,7 @@ def check_xdg_env():
     :param: None
     :returns: Bool
     """
-    if os.environ("$XDG_CONFIG_HOME"):
+    if os.environ.get('XDG_CONFIG_HOME'):
         return True
     else:
+        return False
--- test/__init__.py
+++ test/__init__.py
@@ -1,10 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Requires setuptools to declare namespace package."""
-import pkg_resources
+"""Initialize the package for all scripts used in IPythom startup.
 
+:File: __init__.py
+:Author: Faris Chugthai
+:Email: farischugthai@gmail.com
+:Github: `https://github.com/farisachugthai`_
+
+This module intends to establish a few different things.
+
+- Initialize logging in a general manner
+- Use :mod:`pkg_resources` provided by :mod:`setuptools` in order to
+create the directory as a namespace package
+- Define generic dunder methods
+- Extend the user's $PATH to include this directory even if it != os.cwd
+"""
 import logging
 from logging import NullHandler
+from pkgutil import extend_path
+import sys
+
+import pkg_resources
+
+from __about__ import (
+    __author__,
+    __copyright__,
+    __description__,
+    __docformat__,
+    __email__,
+    __license__,
+    __title__,
+    __package_name__,
+)
+
+logging.getLogger(__name__).addHandler(NullHandler())
 
 pkg_resources.declare_namespace(__name__)
-logging.getLogger(__name__).addHandler(NullHandler())
+
+__path__ = extend_path(sys.path, __file__)

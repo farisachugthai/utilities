daa9cb53050682491f2c482d52e410574c7a9324
farisachugthai <farischugthai@gmail.com>
2019-12-26 06:22:30 -0500

    Update setup and tag as 0.0.2

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# Vim: set fdm=marker:
 """Create an installable package for the utilities repository.
 
 Largely based off of the work done by @kennethreitz in his `setup.py`_
@@ -20,33 +21,41 @@ if sys.version_info[:2] < (3, 6):
 
 # Metadata: {{{1
 
-NAME = 'utilities'
-AUTHOR = "Faris Chugthai",
-EMAIL = "farischugthai@gmail.com",
-DESCRIPTION = "Utiities for maintaining platform agnostic workstations.",
-LICENSE = "MIT",
-KEYWORDS = "linux math science",
-URL = "https://github.com/farisachugthai/utilities",
-REQUIRES_PYTHON = '>=3.6.0'  # actually could be as bad as 3.7+ only.
-VERSION = '0.0.1'
+NAME = "utilities"
+AUTHOR = ("Faris Chugthai",)
+EMAIL = ("farischugthai@gmail.com",)
+DESCRIPTION = ("Utiities for maintaining platform agnostic workstations.",)
+LICENSE = ("MIT",)
+KEYWORDS = ("linux math science",)
+URL = ("https://github.com/farisachugthai/utilities",)
+REQUIRES_PYTHON = ">=3.6.0"  # actually could be as bad as 3.7+ only.
+VERSION = "0.0.2"
 
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
-CONF_PATH = os.path.dirname(os.path.abspath('docs'))
-BUILD_PATH = os.path.join(CONF_PATH, 'build')
-SOURCE_PATH = os.path.join(CONF_PATH, '_source')
+CONF_PATH = os.path.dirname(os.path.abspath("docs"))
+BUILD_PATH = os.path.join(CONF_PATH, "build")
+SOURCE_PATH = os.path.join(CONF_PATH, "_source")
 
-REQUIRED = ['pynvim', 'IPython',]
+REQUIRED = [
+    "pynvim>=0.4.*",
+    "IPython>=7.*",
+]
 
 EXTRAS = {
-    'develop': ['requests', 'flake8', 'flake8-rst', 'yapf'],
-    'docs': [
-        'sphinx',
+    "develop": [
+        "requests",
+        "flake8",
+        "flake8-rst",
+        "yapf"
+    ],
+
+    "docs": [
+        "sphinx>=2.*",
         # Project uses reStructuredText, so ensure that the docutils get
         # installed or upgraded on the target machine
-        'docutils>=0.3',
-        'recommonmark',
-        'numpydoc'
-    ]
+        "docutils>=0.3",
+        "numpydoc>=0.9.1",
+    ],
 }
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -58,27 +67,20 @@ except FileNotFoundError:
     long_description = DESCRIPTION
 
 # Load the package's __version__.py module as a dictionary.
-about = {'__version__': '0.0.1'}
-
-# if not VERSION:
-#     try:
-#         with open(os.path.join(here, NAME, '__version__.py')) as f:
-#             exec(f.read(), about)
-#     except IOError:  # the file doesn't exist
-#         about['__version__'] = None
+about = {"__version__": "0.0.1"}
 
 
 # }}}}
 class UploadCommand(Command):  # {{{1
     """Support setup.py upload."""
 
-    description = 'Build and publish the package.'
+    description = "Build and publish the package."
     user_options = []
 
     @staticmethod
     def status(s):
         """Print output in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+        print("\033[1m{0}\033[0m".format(s))
 
     def initialize_options(self):
         pass
@@ -91,21 +93,20 @@ class UploadCommand(Command):  # {{{1
     def run(self):
         """Upload package."""
         try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
         except OSError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(
-            sys.executable))
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
 
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
 
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(about["__version__"]))
+        os.system("git push --tags")
 
         sys.exit()
 
@@ -114,34 +115,33 @@ class UploadCommand(Command):  # {{{1
 # Where the magic happens: {{{1
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=about["__version__"],
     description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type='text/restructuredtext',
+    long_description_content_type="text/restructuredtext",
     author=AUTHOR,
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=find_packages(exclude=('tests', )),
+    packages=find_packages(exclude=("tests",)),
     # If your package is a single module, use this instead of 'packages':
     # py_modules=['mypackage'],
-
-    # entry_points={
-    #     'console_scripts': ['mycli=mymodule:cli'],
-    # },
+    entry_points={
+        "console_scripts": [
+            "dlink=pyutil.dlink:dlink",
+            "dlink2=pyutil.dlink2:main",
+        ],
+    },
     install_requires=REQUIRED,
     extras_require=EXTRAS,
-
     # don't use setup_requires it's really not well supported
     # setup_requires=['nose>=1.0'],
-
     include_package_data=True,
     package_data={
         # If any package contains *.txt or *.rst files, include them:
-        '': ['*.txt', '*.rst'],
+        "": ["*.txt", "*.rst"],
     },
-    license='MIT',
-
+    license="MIT",
     #  https://www.python.org/dev/peps/pep-0345/#platform-multiple-use
     # A Platform specification describing an operating system supported by the
     # distribution which is not listed in the "Operating System" Trove
@@ -150,17 +150,15 @@ setup(
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
     ],
     # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
+    cmdclass={"upload": UploadCommand, },
     # project home page, if any
     # project_urls={
     #     "Bug Tracker": "https://bugs.example.com/HelloWorld/",
